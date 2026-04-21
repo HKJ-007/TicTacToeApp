1584424d05f2fd6d53a41edf32a0b7f851854b94
sethu <harstudiesh@gmail.com>
2026-04-21 10:50:43 +0530

    UC9: Winning condition check implemented

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,78 +1,50 @@
-import java.util.Random;
-
 public class TicTacToe {
 
     static char[][] board = {
-            {'-', '-', '-'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
+            {'X', 'X', 'X'},
+            {'-', 'O', '-'},
+            {'O', '-', '-'}
     };
 
-    static boolean isHumanTurn = true;
-    static boolean gameOver = false;
-
     public static void main(String[] args) {
-
-        int moves = 0;
-
-        while (!gameOver) {
-
-            if (isHumanTurn) {
-                System.out.println("Human turn");
-                placeMove(0, 0, 'X'); // sample move
-            } else {
-                System.out.println("Computer turn");
-                computerMove();
-            }
-
-            printBoard();
-            moves++;
-
-            // stop condition (basic)
-            if (moves == 9) {
-                gameOver = true;
-                System.out.println("Game Draw!");
-            }
-
-            // switch turn
-            isHumanTurn = !isHumanTurn;
-        }
+        System.out.println(hasWon('X'));
+        System.out.println(hasWon('O'));
     }
 
-    public static boolean isValidMove(int row, int col) {
-        if (row < 0 || row > 2 || col < 0 || col > 2) return false;
-        if (board[row][col] != '-') return false;
-        return true;
-    }
+    // UC9 Logic
+    public static boolean hasWon(char symbol) {
 
-    public static void placeMove(int row, int col, char symbol) {
-        if (isValidMove(row, col)) {
-            board[row][col] = symbol;
-        }
-    }
-
-    public static void computerMove() {
-        Random rand = new Random();
-        int row, col;
-
-        while (true) {
-            int slot = rand.nextInt(9) + 1;
-            row = (slot - 1) / 3;
-            col = (slot - 1) % 3;
-
-            if (isValidMove(row, col)) {
-                placeMove(row, col, 'O');
-                break;
-            }
-        }
-    }
-
-    public static void printBoard() {
+        // check rows
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
+            if (board[i][0] == symbol &&
+                    board[i][1] == symbol &&
+                    board[i][2] == symbol) {
+                return true;
             }
-            System.out.println();
         }
+
+        // check columns
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == symbol &&
+                    board[1][j] == symbol &&
+                    board[2][j] == symbol) {
+                return true;
+            }
+        }
+
+        // check diagonals
+        if (board[0][0] == symbol &&
+                board[1][1] == symbol &&
+                board[2][2] == symbol) {
+            return true;
+        }
+
+        if (board[0][2] == symbol &&
+                board[1][1] == symbol &&
+                board[2][0] == symbol) {
+            return true;
+        }
+
+        return false;
     }
 }
